8ff671d3f474628a5c7dc0df00a843eb40fb6f46
degoldschmidt <goldschmidt.de@gmail.com>
2018-01-19 18:09:11 +0000

    new stuff

---
+++ scripts/test_cr.py
@@ -0,0 +1,16 @@
+from flyquery import flyquery as flyq
+import pygsheets
+
+gc = pygsheets.authorize()
+# Open spreadsheet and then workseet
+sh = gc.open('Stocks Ricardo.xls')
+wks = sh.sheet1
+df = wks.get_as_df()
+# Update a cell with value (just to let him know values is updated ;) )
+#wks.update_cell('A1', "Hey yank this numpy array")
+
+# update the sheet with array
+#wks.update_cells('A2', my_nparray.to_list())
+
+# share the sheet with your friend
+#sh.share("myFriend@gmail.com")
